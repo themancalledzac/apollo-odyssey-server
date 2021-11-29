418c0996d5cbfd48856474bd0711a159d02964a4
Zac Edens <edens.zac@gmail.com>
2021-11-29 10:47:34 -0800

    updated our length to be durationInSeconds. depreciated length in both Track and Module schemas

--- src/schema.js
+++ src/schema.js
@@ -36,8 +36,10 @@ const typeDefs = gql`
     author: Author!
     "the track's main illustration ti display in track card or track page detail"
     thumbnail: String
-    "the track's approximate length to complete, in minutes"
-    length: Int
+    "the track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
     "the number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in Markdown format"
@@ -52,8 +54,10 @@ const typeDefs = gql`
     id: ID!
     "The Module's title"
     title: String!
-    "The Module's length in minutes"
-    length: Int
+    "The Module's length in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    "The module's video duration, in seconds"
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
